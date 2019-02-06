b8c4df51101f1f7b0c7cce8b96bc757a7315b569
AlexDemin <45641409+AlexDemin@users.noreply.github.com>
2019-02-06 17:25:46 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+from outFile import answer
+
+if __name__ == '__main__':
+    print(answer("3+7*(83-75)/7+(-5)"))
+    print(answer("45*pi/e+2"))
+    print(answer("sin(\pi/2)/cos(pi/3)"))
+    print(answer("sin(0) + tg(pi/3) + arccos(sqrt(3/4))"))
+    print(answer("log(e^arcsin(1))"))
+    print(answer("lg(\e^tan(sqrt(pi)))"))
+    print(answer("ctg(pi/3)*cot(pi/6)/cot(7*e/4)"))
+    print(answer("arcsin[-0.5]*{3-pi}"))
+    print(answer("acos{-0.78}/(76-5*[4+11])"))
+    print(answer("asin(e/\pi)+arctg(5)"))
+    print(answer("atg2-atan[5]"))
+    print(answer("arcctg(-3)+arcctan(e^{45})"))
+    print(answer("7.8-e*ln(2)"))
+    print(answer("ln(2)"))
+    print(answer("lg{7*sin(45)}/5*arctan8"))
+    print("done")
